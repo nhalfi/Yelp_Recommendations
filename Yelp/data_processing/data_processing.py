1bfac747b4839d9e454f7ddfeac3bf4438f9981f
nhalfi <nhalfin@uw.edu>
2021-03-10 18:37:04 -0800

    added new functions to clean up main

--- Yelp/data_processing/data_processing.py
+++ Yelp/data_processing/data_processing.py
@@ -1,4 +1,4 @@
-# Module: DataPreprocessing.py
+# Module: data_processing.py
 # Purpose: clean, transform, and prepare data for modeling
 
 #import libraries
@@ -10,9 +10,82 @@ import numpy as np
 import json
 import pandas as pd
 
+
 class ProcessingError(Exception):
     #raised if output from text_process produces an empty dataframe
     pass
+
+def load_datasets(yp_business_path, yp_photo_path, nutritionix_path):
+    """
+    -function to load raw datasets into dataframes
+    -returns dataframes to be fed into subsequent functions below
+    """
+
+    #load yelp datasets; due to GitHub file size constraints, these files must be downloaded locally; see instructions in README
+    try:
+        df_business = pd.read_json(yp_business_path,lines=True)  #update path
+        df_photos = pd.read_json(yp_photo_path, lines=True)
+    except Exception:
+        print("Please double check that you have locally downloaded the Yelp Academic dataset (see README for details).")
+    
+
+    #load training data from Nutritionix API
+    #this dataset was extracted via a PowerShell script; see DownloadNutritionixData.ps1 in the DataProcessing folder and the README instructions
+    try:
+        with open(nutritionix_path,'r') as f:
+            data=f.read()
+    except Exception:
+        print("Please double check that you have locally downloaded the Nutritionix dataset (see README for details).")
+    else:
+        obj = json.loads(data)
+        df_nutritionix = pd.DataFrame.from_dict(obj, orient='columns')
+        df_nutritionix = pd.json_normalize(obj)
+
+    return(df_business,df_photos,df_nutritionix)
+
+def clean_yelp(df_business, df_photos):
+    """
+    -function that contains logic filter and join yelp datasets
+    -df_business: input dataframe that contains Yelp business data
+    -df_photos: input dataframe that contains Yelp photo data
+    -returns cleaned and joined dataframe
+    """
+    
+    df_business['restaurant'] = df_business['categories'].str.contains('Restaurants|Food') 
+    df_restaurants = df_business[df_business['restaurant']==True]
+
+    #drop unneeded columns and output to csv
+    df_restaurants = df_restaurants.drop(['is_open','attributes','hours','restaurant'],axis = 1)
+    #df_restaurants.to_csv('../data/yelp_business_clean.csv')
+
+    #### photo filtering ####
+    df_photos = df_photos[df_photos['business_id'].isin(df_restaurants['business_id'])]
+    df_photos = df_photos[df_photos['label']=="food"]
+    df_photos = df_photos[df_photos['caption']!=""] # exclude records with blank captions
+    #output to csv
+    #df_photos.to_csv('../data/yelp_photos_clean.csv')
+
+    #join dataframes on business_id
+    df_join = df_photos.merge(df_restaurants,how='inner')
+
+    return(df_join)
+
+            
+def calculate_health(df):
+    """
+    -function that contains logic to calculate healthy boolean flag for training data
+    -df: input dataframe that contains restaurant menu items and nutritional information
+    -returns dataframe with calculated healthy column
+    """
+    
+    df2 = df.copy()
+    conditions = [(df2['fields.nf_calories'] >500)|(df2['fields.nf_calories_from_fat'] > 200)|(df2['fields.nf_total_fat']> 20) 
+    | (df2['fields.nf_saturated_fat'] > 8) | (df2['fields.nf_trans_fatty_acid'] >0) | (df2['fields.nf_cholesterol'] > 100) 
+    | (df2['fields.nf_sodium']> 766) | (df2['fields.nf_sugars']> 30)]
+    choices = [0]
+    df2['healthy'] = np.select(conditions, choices, default=1)
+
+    return(df2)
 
 def text_process(df, token_col):
     """
@@ -44,86 +117,45 @@ def text_process(df, token_col):
     else:
         #return tokenized dataframe
         return(df2)
-            
-def calculate_health(df):
+
+def output_csv(yelp_join,yelp_tokenized, nutritionix, nutritionix_tokenized):
     """
-    -function that contains logic to calculate healthy boolean flag for training data
-    -df: input dataframe that contains restaurant menu items and nutritional information
-    -returns dataframe with calculated healthy column
+    -function that outputs intermediate files to csv 
+    -yelp_join: dataframe returned by clean_yelp
+    -yelp_tokenized: dataframe returned by text_process on yelp_join
+    -nutritionix: dataframe returned by calculate_health
+    -nutritionix_tokenized: dataframe returned by text_process on nutritionix
     """
-    
-    df2 = df.copy()
-    conditions = [(df2['fields.nf_calories'] >500)|(df2['fields.nf_calories_from_fat'] > 200)|(df2['fields.nf_total_fat']> 20) 
-    | (df2['fields.nf_saturated_fat'] > 8) | (df2['fields.nf_trans_fatty_acid'] >0) | (df2['fields.nf_cholesterol'] > 100) 
-    | (df2['fields.nf_sodium']> 766) | (df2['fields.nf_sugars']> 30)]
-    choices = [0]
-    df2['healthy'] = np.select(conditions, choices, default=1)
 
-    return(df2)
+    yelp_join.to_csv('../data/yelp_joined_clean.csv')
+    yelp_tokenized.to_csv('../data/yelp_final_tokenized.csv')
+    nutritionix.to_csv('../data/nutritionix_health.csv', index=False)
+    nutritionix_tokenized.to_csv('../data/nutritionix_tokenized.csv')
 
-def clean_yelp(df_business, df_photos):
-    """
-    -function that contains logic filter and join yelp datasets
-    -df_business: input dataframe that contains Yelp business data
-    -df_photos: input dataframe that contains Yelp photo data
-    -returns cleaned and joined dataframe
-    """
-    
-    df_business['restaurant'] = df_business['categories'].str.contains('Restaurants|Food') 
-    df_restaurants = df_business[df_business['restaurant']==True]
-
-    #drop unneeded columns and output to csv
-    df_restaurants = df_restaurants.drop(['is_open','attributes','hours','restaurant'],axis = 1)
-    #df_restaurants.to_csv('../data/yelp_business_clean.csv')
-
-    #### photo filtering ####
-    df_photos = df_photos[df_photos['business_id'].isin(df_restaurants['business_id'])]
-    df_photos = df_photos[df_photos['label']=="food"]
-    df_photos = df_photos[df_photos['caption']!=""] # exclude records with blank captions
-    #output to csv
-    #df_photos.to_csv('../data/yelp_photos_clean.csv')
-
-    #join dataframes on business_id
-    df_join = df_photos.merge(df_restaurants,how='inner')
-
-    return(df_join)
 
 def main():
-    #convert JSON files to Pandas dataframes
-    #due to GitHub file size constraints, these files must be downloaded locally; see instructions in README
     try:
-        df_business = pd.read_json('../data/yelp_academic_dataset_business.json',lines=True) 
-        df_photos = pd.read_json("../data/photos.json", lines=True)
-    except Exception:
-        print("Please double check that you have locally downloaded the Yelp Academic dataset (see README for details).")
+        #load datasets
+        df_business,df_photos,df_nutritionix = load_datasets('../data/yelp_academic_dataset_business.json',"../data/photos.json", '../data/restaurants_items.json')
 
-    #filter and join data
-    df_join = clean_yelp(df_business,df_photos)
-    df_join.to_csv('../data/yelp_joined_clean.csv')
+        #filter and join data
+        df_join = clean_yelp(df_business,df_photos)
 
-    #preprocess joined dataset
-    df_yelp_tokenized = text_process(df_join, 'caption')
-    #output to csv
-    df_yelp_tokenized.to_csv('../data/yelp_final_tokenized.csv')
-    
-    #load training data from Nutritionix API
-    #this dataset was extracted via a PowerShell script; see DownloadNutritionixData.ps1 in the DataProcessing folder and the README instructions
-    try:
-        with open('../data/restaurants_items.json','r') as f:
-            data=f.read()
-    except Exception:
-        print("Please double check that you have locally downloaded the Nutritionix dataset (see README for details).")
+        #preprocess joined dataset
+        df_yelp_tokenized = text_process(df_join, 'caption')
 
-    obj = json.loads(data)
-    df_nutritionix = pd.DataFrame.from_dict(obj, orient='columns')
-    df_nutritionix = pd.json_normalize(obj)
+        #create calculated fields for training dataset
+        df_nutritionix_health = calculate_health(df_nutritionix)
 
-    df_nutritionix_health = calculate_health(df_nutritionix)
-    df_nutritionix_health.to_csv('../data/nutritionix_health.csv', index=False)
+        #preprocess training dataset
+        df_nutritionix_tokenized = text_process(df_nutritionix_health, 'fields.item_name')
 
-    #preprocess training dataset
-    df_nutritionix_tokenized = text_process(df_nutritionix_health, 'fields.item_name')
-    df_nutritionix_tokenized.to_csv('../data/nutritionix_tokenized.csv')
+        #output dataframes to csv
+        output_csv(df_join, df_yelp_tokenized, df_nutritionix_health, df_nutritionix_tokenized)
+    except Exception as e:
+        #print error message associated with e
+        print(e)
+        
 
 if __name__ == "__main__":
     main()
